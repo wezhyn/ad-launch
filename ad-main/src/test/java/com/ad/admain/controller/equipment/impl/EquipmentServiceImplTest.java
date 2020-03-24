21c7076674e06823f5053093fb35486c90174173
wezhyn <wezhyn@163.com>
2020-03-25 07:45:50 +0800

    修改参数

--- ad-main/src/test/java/com/ad/admain/controller/equipment/impl/EquipmentServiceImplTest.java
+++ ad-main/src/test/java/com/ad/admain/controller/equipment/impl/EquipmentServiceImplTest.java
@@ -2,11 +2,14 @@ package com.ad.admain.controller.equipment.impl;
 
 import com.ad.admain.controller.equipment.EquipmentService;
 import com.ad.admain.controller.equipment.entity.Equipment;
+import com.ad.admain.controller.equipment.entity.EquipmentVerify;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
 
 /**
  * @author wezhyn
@@ -21,12 +24,22 @@ public class EquipmentServiceImplTest {
 
     @Test
     public void insert() {
-        Equipment feed=Equipment.builder()
-                .uid(1)
-                .feedback("feed")
-                .status(false)
-                .build();
-        equipmentService.save(feed);
+        for (int count=0; count < 10; count++) {
+            Equipment feed=Equipment.builder()
+                    .uid(1)
+                    .feedback("feed")
+                    .intro("测试")
+                    .createTime(LocalDateTime.of(2020, 1, 1, 1, 1))
+                    .startTime(LocalDateTime.of(2020, 1, 1, 1, 1))
+                    .endTime(LocalDateTime.of(2029, 12, 30, 23, 59))
+                    .verify(EquipmentVerify.PASSING_VERIFY)
+                    .name("test-equp-" + count)
+                    .key("11111111111110" + count)
+                    .remain(0)
+                    .status(false)
+                    .build();
+            equipmentService.save(feed);
+        }
 
     }
 
