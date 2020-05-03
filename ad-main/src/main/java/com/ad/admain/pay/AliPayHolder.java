6036be2efb4d6f234979c1b06b77735e00ed1ad8
wezhyn <wezhyn@163.com>
2020-05-03 17:48:53 +0800

    懒单例

--- ad-main/src/main/java/com/ad/admain/pay/AliPayHolder.java
+++ ad-main/src/main/java/com/ad/admain/pay/AliPayHolder.java
@@ -34,43 +34,55 @@ public final class AliPayHolder {
     public static AlipayClient ALI_PAY_CLIENT;
 
     static {
-        switch (AliPayProperties.CERTIFICATE_TYPE) {
-            case CERTIFICATE: {
-                //构造client
-                CertAlipayRequest certAlipayRequest=new CertAlipayRequest();
-                certAlipayRequest.setServerUrl(AliPayProperties.SERVER_URL);
-                certAlipayRequest.setPrivateKey(AliPayProperties.APP_PRIVATE_KEY);
-                certAlipayRequest.setAppId(AliPayProperties.APP_ID);
-                //设置请求格式，固定值json
-                certAlipayRequest.setFormat("json");
-                certAlipayRequest.setCharset(AliPayProperties.CHARSET);
-                certAlipayRequest.setSignType(AliPayProperties.SIGN_TYPE);
-                certAlipayRequest.setCertPath(AliPayProperties.CERT_PATH);
-                certAlipayRequest.setRootCertPath(AliPayProperties.ROOT_CERT_PATH);
-                certAlipayRequest.setAlipayPublicCertPath(AliPayProperties.PUBLIC_CERT_PATH);
-                try {
-                    ALI_PAY_CLIENT=new DefaultAlipayClient(certAlipayRequest);
-                } catch (AlipayApiException e) {
-                    throw new RuntimeException(e);
+    }
+
+    public static AlipayClient instance() {
+        AlipayClient client = ALI_PAY_CLIENT;
+        if (client == null) {
+            synchronized (AlipayClient.class) {
+                client = ALI_PAY_CLIENT;
+                if (ALI_PAY_CLIENT == null) {
+                    switch (AliPayProperties.CERTIFICATE_TYPE) {
+                        case CERTIFICATE: {
+                            //构造client
+                            CertAlipayRequest certAlipayRequest = new CertAlipayRequest();
+                            certAlipayRequest.setServerUrl(AliPayProperties.SERVER_URL);
+                            certAlipayRequest.setPrivateKey(AliPayProperties.APP_PRIVATE_KEY);
+                            certAlipayRequest.setAppId(AliPayProperties.APP_ID);
+                            //设置请求格式，固定值json
+                            certAlipayRequest.setFormat("json");
+                            certAlipayRequest.setCharset(AliPayProperties.CHARSET);
+                            certAlipayRequest.setSignType(AliPayProperties.SIGN_TYPE);
+                            certAlipayRequest.setCertPath(AliPayProperties.CERT_PATH);
+                            certAlipayRequest.setRootCertPath(AliPayProperties.ROOT_CERT_PATH);
+                            certAlipayRequest.setAlipayPublicCertPath(AliPayProperties.PUBLIC_CERT_PATH);
+                            try {
+                                client = ALI_PAY_CLIENT = new DefaultAlipayClient(certAlipayRequest);
+                            } catch (AlipayApiException e) {
+                                throw new RuntimeException(e);
+                            }
+                            break;
+                        }
+                        case PRIVATE_KEY: {
+                            client = ALI_PAY_CLIENT = new DefaultAlipayClient(
+                                    AliPayProperties.SERVER_URL,
+                                    AliPayProperties.APP_ID,
+                                    AliPayProperties.APP_PRIVATE_KEY,
+                                    "json",
+                                    AliPayProperties.CHARSET,
+                                    AliPayProperties.ALIPAY_PUBLIC_KEY,
+                                    AliPayProperties.SIGN_TYPE
+                            );
+                            break;
+                        }
+                        default: {
+                            throw new RuntimeException("不支持的类型");
+                        }
+                    }
                 }
-                break;
-            }
-            case PRIVATE_KEY: {
-                ALI_PAY_CLIENT=new DefaultAlipayClient(
-                        AliPayProperties.SERVER_URL,
-                        AliPayProperties.APP_ID,
-                        AliPayProperties.APP_PRIVATE_KEY,
-                        "json",
-                        AliPayProperties.CHARSET,
-                        AliPayProperties.ALIPAY_PUBLIC_KEY,
-                        AliPayProperties.SIGN_TYPE
-                );
-                break;
-            }
-            default: {
-                throw new RuntimeException("不支持的类型");
             }
         }
+        return client;
     }
 
     /**
@@ -81,14 +93,14 @@ public final class AliPayHolder {
      * @return true: 验签成功
      */
     public static Map<String, String> sraCheck(Map<String, String[]> zfbAsyncParams) {
-        Map<String, String> parseMap=new HashMap<>(17);
-        if (zfbAsyncParams==null || zfbAsyncParams.size()==0) {
+        Map<String, String> parseMap = new HashMap<>(17);
+        if (zfbAsyncParams == null || zfbAsyncParams.size() == 0) {
             parseMap.put("verify", "false");
             return parseMap;
         }
         for (String name : zfbAsyncParams.keySet()) {
-            String[] values=zfbAsyncParams.get(name);
-            String valueStr=Stream.of(values)
+            String[] values = zfbAsyncParams.get(name);
+            String valueStr = Stream.of(values)
                     .filter(Objects::nonNull)
                     .collect(Collectors.joining(","));
             parseMap.put(name, valueStr);
@@ -97,10 +109,10 @@ public final class AliPayHolder {
         boolean isVerify;
         try {
 
-            isVerify=AlipaySignature.rsaCertCheckV1(parseMap, AliPayProperties.PUBLIC_CERT_PATH,
+            isVerify = AlipaySignature.rsaCertCheckV1(parseMap, AliPayProperties.PUBLIC_CERT_PATH,
                     AliPayProperties.CHARSET, AliPayProperties.SIGN_TYPE);
         } catch (AlipayApiException e) {
-            isVerify=false;
+            isVerify = false;
         }
         parseMap.put("verify", String.valueOf(isVerify));
         return parseMap;
@@ -117,12 +129,12 @@ public final class AliPayHolder {
      */
     public static <T, R extends AlipayTradeAppPayModel> String signZfb(T signOrder, Function<T, R> orderMapper) {
         try {
-            AlipayTradeAppPayRequest createRequest=new AlipayTradeAppPayRequest();
+            AlipayTradeAppPayRequest createRequest = new AlipayTradeAppPayRequest();
 
             createRequest.setBizModel(orderMapper.apply(signOrder));
             createRequest.setNotifyUrl(AliPayProperties.CALLBACK_NOTIFY_URL);
             //这里和普通的接口调用不同，使用的是sdkExecute
-            AlipayTradeAppPayResponse response=ALI_PAY_CLIENT.sdkExecute(createRequest);
+            AlipayTradeAppPayResponse response = instance().sdkExecute(createRequest);
             //就是orderString 可以直接给客户端请求，无需再做处理。
             if (response.isSuccess()) {
                 return response.getBody();
@@ -135,11 +147,11 @@ public final class AliPayHolder {
     }
 
     public static <T, R extends AlipayFundTransUniTransferModel> WithDrawNotification handleWithDraw(T order, Function<T, R> orderMapper) throws WithdrawException {
-        final R model=orderMapper.apply(order);
-        AlipayFundTransUniTransferRequest request=new AlipayFundTransUniTransferRequest();
+        final R model = orderMapper.apply(order);
+        AlipayFundTransUniTransferRequest request = new AlipayFundTransUniTransferRequest();
         request.setBizModel(model);
         try {
-            AlipayFundTransUniTransferResponse response=ALI_PAY_CLIENT.certificateExecute(request);
+            AlipayFundTransUniTransferResponse response = ALI_PAY_CLIENT.certificateExecute(request);
             return new WithDrawResponse(response);
         } catch (AlipayApiException e) {
             throw new WithdrawException(e);
@@ -148,11 +160,11 @@ public final class AliPayHolder {
     }
 
     public static <T, R extends AlipayTradeRefundModel> RefundNotification refundAmount(T order, Function<T, R> orderMapper) throws RefundException {
-        final R model=orderMapper.apply(order);
-        AlipayTradeRefundRequest request=new AlipayTradeRefundRequest();
+        final R model = orderMapper.apply(order);
+        AlipayTradeRefundRequest request = new AlipayTradeRefundRequest();
         request.setBizModel(model);
         try {
-            AlipayTradeRefundResponse response=ALI_PAY_CLIENT.certificateExecute(request);
+            AlipayTradeRefundResponse response = instance().certificateExecute(request);
             return new RefundResponse(response);
         } catch (AlipayApiException e) {
             throw new RefundException(e);
