18d93571c4e39234f63376d4e8cef9c6da5290ce
wezhyn <wezhyn@163.com>
2020-05-09 19:19:55 +0800

    超时时间修改

--- screen-server/src/main/java/com/ad/screen/server/server/ScreenChannelInitializer.java
+++ screen-server/src/main/java/com/ad/screen/server/server/ScreenChannelInitializer.java
@@ -31,18 +31,18 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Component
 @Slf4j
 public class ScreenChannelInitializer extends io.netty.channel.ChannelInitializer<SocketChannel> {
-    public static final AttributeKey<AtomicBoolean> FIRST_READ_CHANNEL=AttributeKey.valueOf("channel_first_read");
+    public static final AttributeKey<AtomicBoolean> FIRST_READ_CHANNEL = AttributeKey.valueOf("channel_first_read");
 
-    public static final AttributeKey<ScheduledFuture<?>> SCHEDULED_SEND=AttributeKey.valueOf("SCHEDULED_SEND_EVENT");
+    public static final AttributeKey<ScheduledFuture<?>> SCHEDULED_SEND = AttributeKey.valueOf("SCHEDULED_SEND_EVENT");
     /**
      * 设备attr的key,{@link ScreenChannelInitializer#channelRead} 处设置
      */
-    public static final AttributeKey<String> IEMI=AttributeKey.valueOf("EQUIP_IEMI");
-    public static final AttributeKey<PooledIdAndEquipCache> POOLED_EQUIP_CACHE=AttributeKey.valueOf("EQUIP_POOLED_CACHE");
+    public static final AttributeKey<String> IEMI = AttributeKey.valueOf("EQUIP_IEMI");
+    public static final AttributeKey<PooledIdAndEquipCache> POOLED_EQUIP_CACHE = AttributeKey.valueOf("EQUIP_POOLED_CACHE");
     /**
      * 5分钟内调度频率
      */
-    public static final Integer SCHEDULE_NUM=25;
+    public static final Integer SCHEDULE_NUM = 25;
     private final HeartBeatMsgMsgHandler heartBeatMsgHandler;
     private final GpsMsgMsgHandler gpsMsgHandler;
     private final ConfirmMsgHandler confirmMsgHandler;
@@ -51,22 +51,22 @@ public class ScreenChannelInitializer extends io.netty.channel.ChannelInitialize
     private final CompensateHandler compensateHandler;
 
     public ScreenChannelInitializer(HeartBeatMsgMsgHandler heartBeatMsgHandler, GpsMsgMsgHandler gpsMsgHandler, ConfirmMsgHandler confirmMsgHandler, ScreenProtocolCheckInboundHandler screenProtocolCheckInboundHandler, CompleteMsgHandler completeMsgHandler, CompensateHandler compensateHandler) {
-        this.heartBeatMsgHandler=heartBeatMsgHandler;
-        this.gpsMsgHandler=gpsMsgHandler;
-        this.confirmMsgHandler=confirmMsgHandler;
-        this.screenProtocolCheckInboundHandler=screenProtocolCheckInboundHandler;
-        this.completeMsgHandler=completeMsgHandler;
-        this.compensateHandler=compensateHandler;
+        this.heartBeatMsgHandler = heartBeatMsgHandler;
+        this.gpsMsgHandler = gpsMsgHandler;
+        this.confirmMsgHandler = confirmMsgHandler;
+        this.screenProtocolCheckInboundHandler = screenProtocolCheckInboundHandler;
+        this.completeMsgHandler = completeMsgHandler;
+        this.compensateHandler = compensateHandler;
     }
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
-        final Channel chChannel=ch.pipeline().channel();
+        final Channel chChannel = ch.pipeline().channel();
         chChannel.attr(FIRST_READ_CHANNEL).set(new AtomicBoolean(false));
         ch.pipeline().addLast(new ScreenProtocolOutEncoder());
         ch.pipeline().addLast(new LineBasedFrameDecoder(60, true, true));
         ch.pipeline().addLast(screenProtocolCheckInboundHandler);
-        ch.pipeline().addLast(new ReadTimeoutHandler(80));
+        ch.pipeline().addLast(new ReadTimeoutHandler(130));
         ch.pipeline().addLast(heartBeatMsgHandler);
         ch.pipeline().addLast(gpsMsgHandler);
         ch.pipeline().addLast(confirmMsgHandler);
@@ -74,27 +74,27 @@ public class ScreenChannelInitializer extends io.netty.channel.ChannelInitialize
         ch.pipeline().addLast(compensateHandler);
 
 
-        final ScheduledFuture<?> scheduledSend=ch.eventLoop().scheduleAtFixedRate(()->{
+        final ScheduledFuture<?> scheduledSend = ch.eventLoop().scheduleAtFixedRate(() -> {
                     try {
-                        final AtomicBoolean channelInitCompleted=chChannel.attr(FIRST_READ_CHANNEL).get();
-                        if (chChannel.attr(SCHEDULED_SEND).get()==null || channelInitCompleted==null ||
+                        final AtomicBoolean channelInitCompleted = chChannel.attr(FIRST_READ_CHANNEL).get();
+                        if (chChannel.attr(SCHEDULED_SEND).get() == null || channelInitCompleted == null ||
                                 !channelInitCompleted.get()) {
 //                            客户端被关闭| 客户端还未被正常初始化
                             return;
                         }
-                        final PooledIdAndEquipCache equipCache=chChannel.attr(POOLED_EQUIP_CACHE).get();
-                        AdEquipment equipment=equipCache.getEquipment();
+                        final PooledIdAndEquipCache equipCache = chChannel.attr(POOLED_EQUIP_CACHE).get();
+                        AdEquipment equipment = equipCache.getEquipment();
                         //若任务表内的数据不为空则发送数据
-                        Map<Integer, Task> received=equipCache.getAllTask();
-                        if (received.size()==0) {
+                        Map<Integer, Task> received = equipCache.getAllTask();
+                        if (received.size() == 0) {
                             log.debug("id为:{}的设备还没收到任务", equipment.getKey());
                             return;
                         }
                         //遍历检查是否有新未发送的task,有则更新任务列表后空白帧的信息
                         for (Map.Entry<Integer, Task> entry : received.entrySet()) {
-                            Task task=entry.getValue();
-                            if (task.getPreTask()==null) {
-                                FixedTask fixedTask=new FixedTask(task);
+                            Task task = entry.getValue();
+                            if (task.getPreTask() == null) {
+                                FixedTask fixedTask = new FixedTask(task);
                                 task.setPreTask(fixedTask);
                                 //将消息推送到设备上
                                 chChannel.writeAndFlush(createResponse(fixedTask, equipment.getKey()));
@@ -104,7 +104,7 @@ public class ScreenChannelInitializer extends io.netty.channel.ChannelInitialize
                         e.printStackTrace();
                     }
                 }
-                , 20, 30, TimeUnit.SECONDS
+                , 0, 300, TimeUnit.SECONDS
         );
 
         chChannel.attr(SCHEDULED_SEND).set(scheduledSend);
