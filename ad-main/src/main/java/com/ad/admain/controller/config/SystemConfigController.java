694432d55c0d083a11c060bffa6624199b016ca6
wezhyn <wezhyn@163.com>
2020-06-17 15:19:39 +0800

    收益调整2

--- ad-main/src/main/java/com/ad/admain/controller/config/SystemConfigController.java
+++ ad-main/src/main/java/com/ad/admain/controller/config/SystemConfigController.java
@@ -6,6 +6,9 @@ import com.wezhyn.project.controller.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author wezhyn
  * @since 05.16.2020
@@ -19,16 +22,30 @@ public class SystemConfigController {
 
     @GetMapping("/revenue")
     public ResponseResult revenueGet() {
+
+        final RevenueConfig config = applicationConfigService.getRevenueConfig();
+        List<Double> revenueList = new ArrayList<>();
+        for (RevenueConfig.RevenueConfigPair revenueConfigPair : config.getAll()) {
+            revenueList.add(revenueConfigPair.getRevenue());
+        }
         return ResponseResult.forSuccessBuilder()
-                .withData("revenue", applicationConfigService.getRevenueConfig().toJson())
+                .withData("revenue", revenueList)
                 .build();
     }
 
     @PostMapping("/revenue")
+    @SuppressWarnings("unchecked")
     public ResponseResult revenue(@RequestBody JSONObject revenue) {
         try {
-            final RevenueConfig config = RevenueConfig.fromJson(revenue.getString("revenue"));
-            applicationConfigService.update(config);
+            final ArrayList<Double> revenueList = revenue.getObject("revenue", ArrayList.class);
+            final RevenueConfig defaultConfig = new RevenueConfig();
+            final RevenueConfig.RevenueConfigPair[] pairs = defaultConfig.getAll();
+            for (int i = 0; i < pairs.length; i++) {
+                if (!pairs[i].getRevenue().equals(revenueList.get(i))) {
+                    pairs[i].setRevenue(revenueList.get(i));
+                }
+            }
+            applicationConfigService.update(defaultConfig);
         } catch (Exception e) {
             return ResponseResult.forFailureBuilder()
                     .withMessage("数据格式错误")
